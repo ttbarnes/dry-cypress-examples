4e8b9e16a9fcc9252bb39976065e404e45496adc
ttbarnes <tony@tonybarnes.me>
2024-10-14 18:44:25 +0100

    chore(server): setup ui directory

---
+++ src/ui/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import path from 'path';
+
+const PORT = 3000;
+
+const app = express();
+
+const __dirname = path.resolve();
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, './src/ui/index.html'))
+);
+
+app.listen(PORT, () => {
+  console.info(`🚀 Server running on port ${PORT}`);
+});
