2e0ed19f8cfc8a761d6002b2fb8c45c7b9e522a0
ttbarnes <tony@tonybarnes.me>
2024-10-15 19:02:41 +0100

    feat(e2e): success page, redirect

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,16 +1,27 @@
 import express from 'express';
+import morgan from 'morgan';
 import path from 'path';
 
 const PORT = 3000;
 
 const app = express();
 
+app.use(morgan('dev'));
+
 const __dirname = path.resolve();
 
 app.use(express.static(path.join(__dirname, './src/ui/public'))); 
 
-app.get('*', (req, res) =>
+app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, './src/ui/index.html'))
+);
+
+app.post('/', (req, res) => {
+  res.redirect('/success');
+});
+
+app.get('/success', (req, res) =>
+  res.sendFile(path.join(__dirname, './src/ui/success.html'))
 );
 
 app.listen(PORT, () => {
